1650e940a3aabf2c7783b0b452d6bbdda2735077
suraj <suraj@Suraj_Chaudhary>
2023-12-16 01:10:55 +0530

    Database Connection

--- Hospital-Management-System/src/main/java/com/db/DBConnect.java
+++ Hospital-Management-System/src/main/java/com/db/DBConnect.java
@@ -1,19 +1,26 @@
 package com.db;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
+import java.sql.*;
+
 
 public class DBConnect {
 
     private static Connection conn;
+	 
 
     public static Connection getConn()
+    
     {
+    	
         try {
+        	
             Class.forName("com.mysql.cj.jdbc.Driver");
+           
             conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/hms", "root", "MySQL@6613");
+            
         } catch (Exception e) {
             e.printStackTrace();
+          
         }
         return conn;
     }
