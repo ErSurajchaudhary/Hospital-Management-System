d9ad1775f40c733557175b1b4b6c5bd302ba3670
suraj <suraj@Suraj_Chaudhary>
2023-12-16 14:11:57 +0530

    Database Connection established

--- Hospital-Management-System/src/main/java/com/db/DBConnect.java
+++ Hospital-Management-System/src/main/java/com/db/DBConnect.java
@@ -13,11 +13,12 @@ public class DBConnect {
     {
     	
         try {
-        	
+        	System.out.println(1);
             Class.forName("com.mysql.cj.jdbc.Driver");
+            System.out.println(2);
            
             conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/hms", "root", "MySQL@6613");
-            
+            System.out.println(conn);
         } catch (Exception e) {
             e.printStackTrace();
           
